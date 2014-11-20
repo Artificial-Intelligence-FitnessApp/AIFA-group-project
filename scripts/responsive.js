5808f682fb65bcc155b29f5136c3af020b3cdb2d
unknown <x14112621@scr3-028.student.nci.col>
2014-11-20 09:03:16 +0000

    update version

---
+++ scripts/responsive.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#menu-trigger").click(function(){
+		$("#navTablet").slideToggle();
+	});
+});
+
+$(document).ready(function(){
+	$(".sub").click(function(){
+		$(".submenu").slideToggle();
+	});
+});	
