4539f3a227c28c7bfbd7e573079936abfe0bd97c
Reccy <aaronmeaney@hotmail.com>
2014-11-22 17:44:40 +0000

    Tidies FAQ code - Working on News

--- AIFA/scripts/faq_script.js
+++ AIFA/scripts/faq_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#faq_content h3').click(function() {
+	$('#faq_section h3').click(function() {
 		$(this).next('.faq_answer').slideToggle(500);
 		$(this).toggleClass('close');
 	});
