e8bcab242c5dbf32f13287c892a3f4e65ab79b0b
Reccy <aaronmeaney@hotmail.com>
2014-11-23 19:39:26 +0000

    News Popup Update

--- AIFA/scripts/news_script.js
+++ AIFA/scripts/news_script.js
@@ -11,25 +11,25 @@ $("#news_bg").click(function(){
 
 /* Change Popup */
 $("#news_one").click(function(){
-	$("#news_popup_header h3").html("Test One");
+	$("#news_popup_header h3").html($("#news_one div h3").html());
 	$("#news_popup_content img").attr("src", $("#news_one img").attr("src"));
-	$("#news_popup_content div").html("<p>T1 P1</p><p>T1 P2</p><p>T1 P3</p>");
+	$("#news_popup_content div").html($("#news_one div p").html());
 });
 
 $("#news_two").click(function(){
-	$("#news_popup_header h3").html("Test Two");
+	$("#news_popup_header h3").html($("#news_two div h3").html());
 	$("#news_popup_content img").attr("src", $("#news_one img").attr("src"));
-	$("#news_popup_content div").html("<p>T2 P1</p><p>T2 P2</p><p>T2 P3</p>");
+	$("#news_popup_content div").html($("#news_two div p").html());
 });
 
 $("#news_three").click(function(){
-	$("#news_popup_header h3").html("Test Three");
+	$("#news_popup_header h3").html($("#news_three div h3").html());
 	$("#news_popup_content img").attr("src", $("#news_one img").attr("src"));
-	$("#news_popup_content div").html("<p>T3 P1</p><p>T3 P2</p><p>T3 P3</p>");
+	$("#news_popup_content div").html($("#news_three div p").html());
 });
 
 $("#news_four").click(function(){
-	$("#news_popup_header h3").html("Test Four");
+	$("#news_popup_header h3").html($("#news_four div h3").html());
 	$("#news_popup_content img").attr("src", $("#news_one img").attr("src"));
-	$("#news_popup_content div").html("<p>T4 P1</p><p>T4 P2</p><p>T4 P3</p>");
+	$("#news_popup_content div").html($("#news_four div p").html());
 });
